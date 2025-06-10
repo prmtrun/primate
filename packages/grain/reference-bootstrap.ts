87e0ed6175ec5b3d1de3410654493adde1e185cd
Joshua Tenner <jtenner@notwindows>
2025-06-10 17:37:19 -0400

    more!

---
+++ packages/grain/reference-bootstrap.ts
@@ -0,0 +1,38 @@
+// import FileRef from "@rcompat/fs/FileRef";
+// import * as wasi from "node:wasi";
+
+
+// const wasiSnapshotPreview1 = new wasi.WASI({
+//   version: "preview1",
+//   env: process.env,
+//   args: process.argv,
+// });
+// const wasmPath = "./test.wasm";
+
+// const wasm = await WebAssembly.instantiate(
+//   typeof Bun === "object"
+//     ? await Bun.file(wasmPath).arrayBuffer()
+//     : await FileRef.arrayBuffer(wasmPath),
+//     {
+//       "wasi_snapshot_preview1": wasiSnapshotPreview1.wasiImport
+//     }
+// );
+
+// wasiSnapshotPreview1.start(wasm.instance);
+
+// for (const [name, _] of Object.entries(wasm.instance.exports)) {
+//   console.log(name);
+// }
+
+// const result = wasm.instance.exports.get(req);
+// const ref = Buffer.from(wasm.instance.exports.memory.buffer, result);
+// console.log(ref);
+
+// wasm.instance.exports._isResponseText(ptr);
+// let optionPtr = wasm.instance.exports._getResponseText(ptr)
+// let responseTextPtr = wasm.instance.exports._expectOption(optionPtr);
+// let responseText = getString(responseTextPtr);
+
+// _decrPtr(optionPtr);
+// _decrPtr(responseTextPtr);
+// _decrPtr(ptr);
--- packages/grain/src/private/build.ts
+++ packages/grain/src/private/build.ts
@@ -1,22 +1,38 @@
+import assert from "@rcompat/invariant/assert";
+import execute from "@rcompat/stdio/execute";
+import FileRef from "@rcompat/fs/FileRef";
 import type { BuildAppHook } from "@primate/core/hook";
 import which from "@rcompat/stdio/which";
-import assert from "@rcompat/invariant/assert";
-import FileRef from "@rcompat/fs/FileRef";
 
-const command = await which("grain");
-const grainIncludeDir = FileRef.join(import.meta.dirname, "include");
+let command: string | null = null;
+let grainIncludeDir: FileRef | null = null;
+let postludeRef: FileRef | null = null;
 
-
-
-const run = (wasm: FileRef, grain: FileRef) =>
-  `${command} build -o ${wasm.name} ${grain.name} -I ${grainIncludeDir}`;
+const compileGrainFile = (wasm: FileRef, grain: FileRef) =>
+  `${command} compile -o ${wasm.name} ${grain.name} -I ${grainIncludeDir}`;
 
 export default (extension: string): BuildAppHook => (app, next) => {
+  
   app.bind(extension, async (grain, context) => {
+    const importGrainRegex = /(\n|\r\n|^)from "primate" include Primate(\n|\r\n|$)/;
+    command ||= await which("grain");
+    grainIncludeDir ||= await FileRef.join(import.meta.url, "include");
+    postludeRef ||= await FileRef.join(import.meta.url, "postlude.gr");
+
     assert(context === "routes", "grain: only route files are supported");
     
+    // TODO: 1. Get module text
+    //       2. Assert `(\n|\r\n|^)from "primate" include Primate(\n|\r\n|$)` exists
     
+    const code = await grain.text();
+    const postlude = await postludeRef.text();
     
+    const outputGrain = importGrainRegex.test(code)
+      ? `${code}\n${postlude}`
+      : `${code}\nfrom "primate" include Primate\n${postlude}`;
+    await grain.write(outputGrain);
+    const wasm = grain.bare(".wasm");
+    await execute(compileGrainFile(wasm, grain), { cwd: `${grain.directory}` });
 
   });
 
