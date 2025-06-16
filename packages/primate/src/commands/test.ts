d2a7fda6bc9be090d2048d02838282a83677ce17
terrablue <102580937+terrablue@users.noreply.github.com>
2025-06-16 23:02:33 +0200

    primate: switch to development mode on the test command

--- packages/primate/src/commands/test.ts
+++ packages/primate/src/commands/test.ts
@@ -27,7 +27,7 @@ const first_error = (left: string, right: string) => {
 };
 
 export default async () => {
-  await build("production", "web");
+  await build("development", "web");
   const app = (await serve()).default;
 
   const files = await (await root()).join(directory)
