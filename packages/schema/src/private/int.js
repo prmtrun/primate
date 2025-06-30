7ced7a39bb2f8fa2dc5c5659e28876b6a125d55b
terrablue <102580937+terrablue@users.noreply.github.com>
2025-07-01 00:49:00 +0200

    remove @primate/schema

--- packages/schema/src/private/int.js
+++
@@ -1,4 +0,0 @@
-import commonint from "#commonint";
-import int from "#predicates/int";
-
-export default commonint(int);
--- packages/schema/src/private/orthrow.js
+++
@@ -1,7 +0,0 @@
-export default (predicate, message) => {
-  try {
-    return predicate();
-  } catch (_) {
-    throw new Error(message);
-  }
-};
